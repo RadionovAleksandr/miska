92dd0aa79cacff7bc8dd2d9ca177ddb403ac6087
Radionov Alexandr <Radionovalek@yandex.ru>
2019-04-02 18:39:46 +0800

    structural tree changes

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
 gulp.task("style", function() {
+  console.log("Запускаю команду");
   gulp.src("less/style.less")
     .pipe(plumber())
     .pipe(less())
