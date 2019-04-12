c53aef3f900ad6d23623759527127fccf9025769
Radionov Alexandr <Radionovalek@yandex.ru>
2019-04-12 12:07:51 +0800

    add mobile menu

--- js/mishka.js
+++ js/mishka.js
@@ -1,19 +1,8 @@
-
-var header = document.querySelector(".page-header");
-var headerSearch = document.querySelector(".page-header__search");
+var header = document.querySelector(".page-header__wrap--top");
 var headerSearchBox = document.querySelector(".page-header__search-box");
-var headerSearchImg = document.querySelector(".page-header__search-img");
-var headerSearchText = document.querySelector(".page-header__search-text");
-
-var menuButton = document.querySelector(".main-nav__toggle")
-var list = document.querySelector(".main-nav__list--top")
-var listItem = document.querySelector(".site-list__item")
-var listItem2 = document.querySelector(".site-list__item2")
-
-
-var basket = document.querySelector(".basket")
-var basketField = document.querySelector(".basket__field")
-var basketImg = document.querySelector(".basket__img")
+var menuButton = document.querySelector(".main-nav__toggle");
+var list = document.querySelector(".main-nav--top");
+var basket = document.querySelector(".basket");
 
 
 
@@ -23,31 +12,17 @@ menuButton.addEventListener("click", function(evt) {
   if (menuButton.classList.contains("main-nav__toggle--closed"))  {
   menuButton.classList.remove("main-nav__toggle--closed");
   menuButton.classList.add("main-nav__toggle--active");
-  list.classList.add("main-nav__list--top-active");
-  listItem.classList.add("site-list__item--active");
-  listItem2.classList.add("site-list__item--active");
-  header.classList.add("page-header--active");
-  headerSearch.classList.add("page-header__search--active");
+  list.classList.add("main-nav--top-active");
+  header.classList.add("page-header__wrap--top-active");
   headerSearchBox.classList.add("page-header__search-box--active");
-  headerSearchImg.classList.add("page-header__search-img--active");
-  headerSearchText.classList.add("page-header__search-text--active");
   basket.classList.add("basket--active");
-  basketField.classList.add("basket__field--active");
-  basketImg.classList.add("basket__img--active");
     } else {
         menuButton.classList.add("main-nav__toggle--closed");
         menuButton.classList.remove("main-nav__toggle--active");
-        list.classList.remove("main-nav__list--top-active");
-        listItem.classList.remove("site-list__item--active");
-        listItem2.classList.remove("site-list__item--active");
-        header.classList.remove("page-header--active");
-        headerSearch.classList.remove("page-header__search--active");
+        list.classList.remove("main-nav--top-active");
+        header.classList.remove("page-header__wrap--top-active");
         headerSearchBox.classList.remove("page-header__search-box--active");
-        headerSearchImg.classList.remove("page-header__search-img--active");
-        headerSearchText.classList.remove("page-header__search-text--active");
         basket.classList.remove("basket--active");
-        basketField.classList.remove("basket__field--active");
-        basketImg.classList.remove("basket__img--active");
             }
 }); 
 
